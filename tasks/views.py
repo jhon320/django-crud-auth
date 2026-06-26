a1c9fe55f0e5dc405587955026d13af0e385f79b
Jhon Angulo <jhonjairoramos2000@gmail.com.com>
2026-06-26 13:55:34 -0500

    Se actualizó el CRUD de tareas y se corrigieron errores

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
